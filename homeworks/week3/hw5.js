7a471fbaf9a29ade1fe6e9b5fcd4d5ca354fb104
李泓伸 <lihongshen@lihongshende-MacBook-Pro.local>
2020-07-07 15:54:00 +0800

    week3-update1

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -14,16 +14,16 @@ function whosBig(result) {
     const num = result[i].split(' ');
     if (num[0].length === num[1].length) { // 第一階：判斷字串長度-->相等
       if (num[0] > num[1]) { // 第二階：陣列0 > 1
-        console.log(num[2] === 1 ? 'A' : 'B'); // 第三階：陣列2為1時的判斷
+        console.log(Number(num[2]) === 1 ? 'A' : 'B'); // 第三階：陣列2為1時的判斷
       } else if (num[0] < num[1]) { // 第二階：陣列1 > 0
-        console.log(num[2] === -1 ? 'A' : 'B'); // 第三階：陣列2為-1時的判斷
+        console.log(Number(num[2]) === -1 ? 'A' : 'B'); // 第三階：陣列2為-1時的判斷
       } else { // 第二階：字串相等，陣列0和陣列1的字典序也相同
         console.log('DRAW');
       }
     } else if (num[0].length > num[1].length) { // 第一階：判斷字串長度-->不相等，故一定不會有'DRAW'發生
-      console.log(num[2] === 1 ? 'A' : 'B');
+      console.log(Number(num[2]) === 1 ? 'A' : 'B');
     } else if (num[0].length < num[1].length) { // 第一階：判斷字串長度-->不相等，故一定不會有'DRAW'發生
-      console.log(num[2] === -1 ? 'A' : 'B');
+      console.log(Number(num[2]) === -1 ? 'A' : 'B');
     }
   }
 }
