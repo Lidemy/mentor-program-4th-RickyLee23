858dbaf2d99467401c17e2d75bc89cb455bab6d6
李泓伸 <lihongshen@lihongshende-MacBook-Pro.local>
2020-10-04 20:24:17 +0800

    week12

---
+++ homeworks/week12/hw2/add_todos.php
@@ -0,0 +1,42 @@
+<?php
+  require_once('conn.php');
+  header('Content-type:application/json;charset=utf-8');//回覆的檔案是 json 格式
+  if (
+    empty($_POST['todo'])
+  ) {
+    $json = array(
+      "ok" => false,
+      "message" => "Please input missing fields"
+    );
+
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $todo = $_POST['todo'];
+
+  $sql = "INSERT INTO RickyLee_todolist (todo) values (?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('s',$todo);
+  $result = $stmt->execute();
+
+  if (!$result) {
+    $json = array(
+      "ok" => false,
+      "message" => $conn->error
+    );
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+    $json = array(
+      "ok" => true,
+      "message" => "success",
+      "id" => $conn->insert_id,
+    );
+    $response = json_encode($json);
+    echo $response;  
+
+  ?>
---
+++ homeworks/week12/hw2/get_todos.php
@@ -0,0 +1,45 @@
+<?php
+  require_once('conn.php');
+  header('Content-type:application/json;charset=utf-8');//回覆的檔案是 json 格式
+  if (
+    empty($_GET['id'])
+  ) {
+    $json = array(
+      "ok" => false,
+      "message" => "Please send id in url"
+    );
+
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+ 
+  $id = intval($_GET['id']);
+
+  $sql = "select * from RickyLee_todolist where id = ? ";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('i',$id);
+  $result = $stmt->execute();
+
+  if (!$result) {
+    $json = array(
+      "ok" => false,
+      "message" => $conn->error
+    );
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $result = $stmt->get_result();
+  $row = $result->fetch_assoc();
+  $json = array (
+      "ok" => true,
+      "data" => array(
+        "id" => $row["id"],
+        "todo" => $row["todo"]
+      )
+    );
+    $response = json_encode($json);
+    echo $response;
+  ?>
